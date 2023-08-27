8f559f669dbfd7d6aa70bfaf5c7fcc6cfa13535e
Igor Maurício de Campos <igor_mcampos@hotmail.com>
2023-08-27 13:37:42 -0300

    Kids With Candies

---
+++ src/main/java/leetcode/automaniac/KidsWithCandies.java
@@ -0,0 +1,22 @@
+package leetcode.automaniac;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class KidsWithCandies {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+
+        int max = candies[0];
+        List<Boolean> result = new ArrayList<>();
+
+        for (int i = 1; i < candies.length; i++) {
+            if (candies[i] > max) {
+                max = candies[i];
+            }
+        }
+        for (int i = 0; i < candies.length; i++) {
+            result.add(candies[i] + extraCandies >= max);
+        }
+        return result;
+    }
+}
