770b98593e03593b01a45ee19c2f3c55d9ffa374
Igor Maurício de Campos <igor_mcampos@hotmail.com>
2023-08-27 13:46:08 -0300

    Can Place Flowers

---
+++ src/test/java/CanPlaceFlowersTest.java
@@ -0,0 +1,69 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import leetcode.automaniac.CanPlaceFlowers;
+
+public class CanPlaceFlowersTest {
+    @Test
+    public void example1() {
+        int[] flowerbed = { 1, 0, 0, 0, 1 };
+        int n = 1;
+
+        boolean expected = true;
+        boolean result = new CanPlaceFlowers().canPlaceFlowers(flowerbed, n);
+
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void example2() {
+        int[] flowerbed = { 1, 0, 0, 0, 1 };
+        int n = 2;
+
+        boolean expected = false;
+        boolean result = new CanPlaceFlowers().canPlaceFlowers(flowerbed, n);
+
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void example3() {
+        int[] flowerbed = { 0, 0, 1, 0, 1 };
+        int n = 2;
+
+        boolean expected = false;
+        boolean result = new CanPlaceFlowers().canPlaceFlowers(flowerbed, n);
+
+        Assert.assertEquals(expected, result);
+    }
+
+}
+
+/*
+ * You have a long flowerbed in which some of the plots are planted, and some
+ * are not. However, flowers cannot be planted in adjacent plots.
+ * 
+ * Given an integer array flowerbed containing 0's and 1's, where 0 means empty
+ * and 1 means not empty, and an integer n, return true if n new flowers can be
+ * planted in the flowerbed without violating the no-adjacent-flowers rule and
+ * false otherwise.
+ * 
+ * 
+ * 
+ * Example 1:
+ * 
+ * Input: flowerbed = [1,0,0,0,1], n = 1
+ * Output: true
+ * Example 2:
+ * 
+ * Input: flowerbed = [1,0,0,0,1], n = 2
+ * Output: false
+ * 
+ * 
+ * Constraints:
+ * 
+ * 1 <= flowerbed.length <= 2 * 104
+ * flowerbed[i] is 0 or 1.
+ * There are no two adjacent flowers in flowerbed.
+ * 0 <= n <= flowerbed.length
+ */
